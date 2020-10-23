e87131b605d4588a36176d033e46e458d33f04f4
Debayan Basu <debayanbasu11@gmail.com>
2020-10-23 19:20:33 +0530

    committing initial project files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public event System.Action OnReachedEndOfLevel;
+
+    public float moveSpeed = 7;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    bool disabled;
+
+    Vector3 velocity;
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Guard.OnGuardHasSpottedPlayer += Disable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputDirection = Vector3.zero;
+        if(!disabled){
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+       
+       velocity = transform.forward * moveSpeed * smoothInputMagnitude; 
+    }
+
+    void Disable(){
+        disabled = true;
+    }
+
+    void FixedUpdate(){
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+    }
+
+    void OnDestroy(){
+        Guard.OnGuardHasSpottedPlayer -= Disable;
+    }
+
+    void OnTriggerEnter(Collider hitCollider)
+    {
+       if(hitCollider.tag == "Finish"){
+           Disable();
+           if(OnReachedEndOfLevel != null){
+               OnReachedEndOfLevel();
+           }
+       }
+    }
+}
